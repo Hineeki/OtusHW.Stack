77cc13522906c9a9e2eda6c46eb89faf9030580b
Hineeki <tanchinsan@gmail.com>
2023-07-16 15:48:11 +1000

    Добавьте файлы проекта.

---
+++ StackHomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StackHomeWork
+{    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pook = new Stack("213", "asd", "qwe", "asd", "zxc");
+            
+            pook.Pop();
+            pook.Add("qwerty");
+            pook.Clear();
+            //pook.Pop();
+            Console.WriteLine(pook.Size);
+            Console.WriteLine(pook.Top);
+        }
+    }
+}
---
+++ StackHomeWork/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackHomeWork
+{
+    internal class Stack
+    {
+        List<string> Strings;
+        public Stack(params string[] strings)
+        {
+            Strings = new List<string>();
+            foreach (string s in strings)
+            {
+                Strings.Add(s);
+            }
+        }
+        public void Add(string s)
+        {
+            Strings.Add(s);
+        }
+        public void Pop()
+        {
+            if (Strings.Count == 0)
+            {
+                throw new EmptyEx("Стек пустой");
+            }
+            Strings.RemoveAt(Strings.Count - 1);
+        }
+        public void Clear()
+        {
+            Strings.Clear();
+        }
+        public int Size
+        {
+            get
+            {                
+                return Strings.Count;
+            }
+        }
+        public string Top
+        {
+            get
+            {
+                if (Strings.Count == 0)
+                {
+                    return null;
+                }
+                return Strings[Strings.Count-1];
+            }
+        }
+    }    
+}
